2c0fdf19ef75648868c2c28cf9eb52ba88cbf8bc
irina <iizquierdo.ieu2023@student.ie.edu>
2025-11-29 11:53:48 +0100

    fixed wsgi

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,6 +1,3 @@
-import os
 from backend.app import create_app
 
-# Lee FLASK_ENV (si existe) para seleccionar la configuración
-config_name = os.environ.get("FLASK_ENV", os.environ.get("FLASK_CONFIG", "production"))
-app = create_app(config_name)
+app = create_app()
